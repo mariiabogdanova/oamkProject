7e5f287422aeaf13ff3a09cd21e301b88393b89e
Unknown <heli.isohatala@gmail.com>
2017-04-07 09:07:03 +0300

    Foter/poll/main page..
    work in progress :)

---
+++ Teacher_student/poll.php
@@ -0,0 +1,14 @@
+<div id="pollBoxes">
+    <div id="pollHeader">Enter the poll header here: </div>
+    <div id="pollQuestions" >
+        <form  action="poll.php" method="post">
+            <label for="question">Ask your question here:</label><br />
+            <input type="radio" name="answerA" value="A">Answer A<br />
+            <input type="radio" name="answerB" value="B">Answer B<br />
+            <input type="radio" name="answerC" value="C">Answer C<br />
+
+
+
+        </form>
+    </div>
+</div>
