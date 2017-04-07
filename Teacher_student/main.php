0393d886d89dd5c92ef3680c04374fa72dc700b7
Unknown <heli.isohatala@gmail.com>
2017-04-07 10:44:10 +0300

    something

--- Teacher_student/main.php
+++ Teacher_student/main.php
@@ -4,7 +4,21 @@ include poll.php ;
 ?>
 <h2>Headlines or news or smth here</h2>
 <br />
-<div id="mainContent"></div>
+<div id="mainContent">
+
+    <button id="teacherNote" style="btnButton-danger" >Teacher's note</button>
+    <button id="studentNews" style="btnButton-danger" >Your news</button>
+    <br />
+       <div="newsContent" > 
+            NEWS! we got something done and smths are making us depressed, byt hey, its all progress! !NEWS
+
+       </div>
+       <br />
+       <div id="videoBox">
+
+           Here one embedds those kickass videos :D
+       </div>
+</div>
 
 
 <?php include footerForAll.php; ?>
